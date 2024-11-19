7e5dbbdedfcc76bb15bd0be972419cd9016158a2
kuraimos <kuraimoss@gmail.com>
2024-11-20 01:55:53 +0700

    update

--- server.js
+++ server.js
@@ -3,11 +3,19 @@
 require('dotenv').config();
 const Hapi = require("@hapi/hapi");
 const Path = require("path");
+const fs = require('fs');
 
 const init = async () => {
+  // Setup HTTPS options if SSL certificate files are provided
+  const tlsOptions = process.env.SSL_KEY && process.env.SSL_CERT ? {
+    key: fs.readFileSync(process.env.SSL_KEY),
+    cert: fs.readFileSync(process.env.SSL_CERT)
+  } : null;
+
   const server = Hapi.server({
-    port: process.env.PORT || 80, 
-    host: "0.0.0.0", 
+    port: process.env.PORT || 80,
+    host: "0.0.0.0",
+    tls: tlsOptions // Enable HTTPS if tlsOptions are provided
   });
 
   await server.register(require("@hapi/inert"));
