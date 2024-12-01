d52669ef354bd329c9dc64b7e0ae9bc6104a339e
kuraimos <kuraimoss@gmail.com>
2024-12-02 04:02:18 +0700

    update

--- python/predict.py
+++ python/predict.py
@@ -1,9 +1,16 @@
-import json
+import sys  # Tambahkan ini di atas
 import tensorflow as tf
 import numpy as np
+import json
 from transformers import BertTokenizer
 
-# Fungsi untuk mempersiapkan data
+# Fungsi untuk memuat model TensorFlow Lite
+def load_model(model_path):
+    interpreter = tf.lite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+    return interpreter
+
+# Fungsi untuk mempersiapkan data (preprocessing)
 def prepare_data(input_text, tokenizer):
     token = tokenizer.encode_plus(
         input_text,
@@ -11,34 +18,46 @@ def prepare_data(input_text, tokenizer):
         truncation=True,
         padding='max_length',
         add_special_tokens=True,
-        return_tensors='np'
+        return_tensors='tf'
     )
-    return token['input_ids'], token['attention_mask']
+    return {
+        'input_ids': tf.cast(token.input_ids, tf.int32),
+        'attention_mask': tf.cast(token.attention_mask, tf.int32)
+    }
 
 # Fungsi untuk melakukan prediksi
-def make_prediction(model, processed_data):
-    input_ids, attention_mask = processed_data
-    input_ids = np.array(input_ids, dtype=np.int32)
-    attention_mask = np.array(attention_mask, dtype=np.int32)
-    
-    # Prediksi
-    input_data = [input_ids, attention_mask]
-    output = model.predict(input_data)
-    
-    # Misalnya output berupa kelas
-    predicted_class = np.argmax(output)
-    
-    # Return sebagai JSON
-    return json.dumps({"predicted_class": str(predicted_class)})
+def make_prediction(model, processed_data, classes=['Awful', 'Poor', 'Neutral', 'Good', 'Awesome']):
+    input_ids = processed_data['input_ids']
+    attention_mask = processed_data['attention_mask']
 
-# Load tokenizer dan model
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-model = tf.lite.Interpreter(model_path="./models/bert_sentiment_model.tflite")
+    input_details = model.get_input_details()
+    output_details = model.get_output_details()
 
-# Ambil input dari argumen
-input_text = sys.argv[1]
-processed_data = prepare_data(input_text, tokenizer)
-result = make_prediction(model, processed_data)
+    model.set_tensor(input_details[0]['index'], input_ids)
+    model.set_tensor(input_details[1]['index'], attention_mask)
 
-# Print hasil dalam format JSON
-print(result)
+    model.invoke()
+
+    output_data = model.get_tensor(output_details[0]['index'])
+    prediction = np.argmax(output_data)
+
+    return classes[prediction]
+
+if __name__ == "__main__":
+    model_path = './models/bert_sentiment_model.tflite'
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+    # Mendapatkan input teks dari argumen
+    input_text = sys.argv[1]
+    
+    # Memuat model
+    model = load_model(model_path)
+    
+    # Memproses data
+    processed_data = prepare_data(input_text, tokenizer)
+
+    # Melakukan prediksi
+    result = make_prediction(model, processed_data)
+
+    # Mengembalikan hasil prediksi
+    print(json.dumps({"sentiment": result}))
